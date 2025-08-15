475c7be8b1cab5ac41213230f9eb3a0af5f7ab18
Sanyal Singh <sanyalsingh84@gmail.com>
2025-08-15 13:19:19 +0530

    Fix Netlify SPA routing issue and backend connection in constant file

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,8 @@
-// export const BASE_URL =
-//   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
-export const BASE_URL = ''; // If using proxy
+export const BASE_URL =
+  process.env.NODE_ENV === 'develeopment'
+    ? 'https://proshop-v2-nq16.onrender.com'
+    : '';
+// export const BASE_URL = ''; // If using proxy
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
